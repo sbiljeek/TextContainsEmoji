844728b5e6d1335561912ec768acf521dcc89ca6
sbiljeek <sbiljeekdev@gmail.com>
2023-02-23 09:28:38 +0300

    Initial Commit

---
+++ TextContainsEmoji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextContainsEmoji
+//
+//  Created by Salman Biljeek on 2/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
